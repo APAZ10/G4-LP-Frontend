9d02285b2c81725fa7de9e63aceec85b267b7f45
KevinB1500 <kdbautis@espol.edu.ec>
2022-01-23 20:02:47 -0500

    Recorridos

---
+++ src/app/services/recorridos/recorridos.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class RecorridosService {
+
+    constructor(
+      private http: HttpClient
+    ) { }
+  
+    list(): Observable<any[]> {
+      return this.http.get<any[]>(`${environment.datababe_url}/recorrido`);
+    }
+  }
